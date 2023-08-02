42f7b1bb8da20247e5d07a183a89113f325bbac7
YASHALAWA8 <dinocowww@gmail.com>
2023-08-02 06:29:15 +0300

    Подключение CSS, добавление картинки на превью

--- block/views.py
+++ block/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Article
 
 def home_page(request):
-    return HttpResponse("""<html>
-    <title>Сайт Андрея Пяткина</title>
-    <h1>Андрей Пяткин</h1>
-    </html>""")
+    article = Article.objects.all()
+    return render(request, 'article/article.html', {'article':article})
